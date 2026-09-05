6fa92b897a68cd5e3afef08784798bb603d54ac4
crisce-03 <cristophercerritos0@gmail.com>
2026-09-05 11:26:30 -0600

    chore: estructura inicial MVC y archivo .gitignore

---
+++ src/main/java/com/mycompany/sistemagestioncine/SistemaGestionCine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestioncine;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SistemaGestionCine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
